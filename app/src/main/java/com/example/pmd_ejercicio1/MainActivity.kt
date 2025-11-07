299a15f1d39c239c069fc15484aa1fbae940a740
nsmirnitskii-hue <n.smirnitskii@ikasle.eus>
2025-11-07 18:13:09 +0100

    comit 2

--- app/src/main/java/com/example/pmd_ejercicio1/MainActivity.kt
+++ app/src/main/java/com/example/pmd_ejercicio1/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.pmd_ejercicio1.ui.theme.PMD_Ejercicio1Theme
@@ -36,44 +37,50 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun Alicacion(){
-    var numero by remember { mutableStateOf("") }
-    var ListaEnteros = remember { mutableListOf<Int>() }
-    var ListaNegativos = remember { mutableListOf<Int>() }
-    var LiostaCero = remember { mutableListOf<Int>() }
+    var numeroEntrada by remember { mutableStateOf("") }
+    var listaEnteros = remember { mutableListOf<Int>() }
+    var listaNegativos = remember { mutableListOf<Int>() }
+    var listaCero = remember { mutableListOf<Int>() }
+    var mensajeError by remember { mutableStateOf("") }
     Column(modifier = Modifier.fillMaxSize()
         .padding(16.dp),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally) {
         Text("Introduce un numero entero positivo, negativo o cero")
-
+        Text("aqui pondremos el mensaje de error",
+            color = Color.Red)
         OutlinedTextField(
-            value = numero,
+            value = numeroEntrada,
             onValueChange = {newNumero ->
-                numero = newNumero },
+                numeroEntrada = newNumero },
             label = {Text("numero")}
         )
         Button(onClick = {
-            InsertarNum(numero, ListaEnteros, ListaNegativos, LiostaCero)
-        }) {Text("Inertsr numero")}
+            // mete el codigo que gestioa la insercion de numeros aqui
+            var numero = numeroEntrada
+            if (numero.isNotEmpty()) {
+                try {
+                    val numEntero = numero.toInt()
+                    if (numEntero > 0) {
+                        listaEnteros.add(numEntero)
+                    } else if (numEntero < 0) {
+                        listaNegativos.add(numEntero)
+                    } else {
+                        listaCero.add(numEntero)
+                    }
+                } catch (e: Exception) {
+                    // el error se tiene que imprimir en ventana en un texto en rojo
+                    // el error tambien se tiene que ver con Toast
+                    println("Caracter no valido")
+                }
+            }else{
+                println("Introduce un numero")
+            }
+        }) {
+            Text("Añadir numero")
+
+        }
     }
 
 }
-@Composable
-fun InsertarNum(num: String, listaEnteros: MutableList<Int>, listaNegativos: MutableList<Int>, listaCero: MutableList<Int>) {
-    if (num.isNotEmpty()) {
-        try {
-            val numEntero = num.toInt()
-            if (numEntero > 0) {
-                listaEnteros.add(numEntero)
-            } else if (numEntero < 0) {
-                listaNegativos.add(numEntero)
-            } else {
-                listaCero.add(numEntero)
-            }
-        } catch (e: Exception) {
-            println("Caracter no valido")
-        }
-    }else{
-        println("Introduce un numero")
-    }
-}
+
